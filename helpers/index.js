9146b705a8fcc3705691bede45c564208af26e11
barthaasbroek <bart@phoenixsite.nl>
2022-04-06 08:47:56 +0200

    remove helpers

--- helpers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    uniquify(arr, compareFn) {
-        compareFn = compareFn ? compareFn : (list, entry) => list.indexOf(entry) === -1;
-        return arr.reduce((list, entry) => {
-            return compareFn(list, entry) ? [...list, entry] : list
-        }, [])
-    }
-}
-
